d660207204f6a325a7194748d6700f8ed413099e
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-02-16 05:15:17 +0200

    asdasd

--- src/App.js
+++ src/App.js
@@ -19,12 +19,25 @@ function App() {
         </div>
         <button className="button">{ticker}</button>
         <div className="socialsBlock">
-          <a href="https://google.com/" target="_blank" rel="noopener noreferrer"><img alt="telegram" src={TG} /></a>
-          <a href="https://google.com/" target="_blank" rel="noopener noreferrer"><img alt="twitter" src={TW} /></a>
+          <a href="https://t.me/SutellaOnSol" target="_blank" rel="noopener noreferrer"><img alt="telegram" src={TG} /></a>
+          <a href="https://twitter.com/SutellaSol" target="_blank" rel="noopener noreferrer"><img alt="twitter" src={TW} /></a>
         </div>
       </div>
       <div className="section2">
-        
+        <h1 className="heading1">the <span className="red">only</span> thing <span className="red">you need</span> is <br/><span className="red">pancakes</span> with <span className="red">sutella</span></h1>
+      </div>
+      <div className="section3">
+        <h2 className="heading2">T<span className="red">OKENOMICS</span></h2>
+        <h3 className="subheading">SUPPLY: <span className="red">8,888,888</span></h3>
+        <h3 className="subheading">SYMBOL: <span className="red">{ticker}</span></h3>
+        <div className="outFlex">
+          <div></div>
+        <div className="flexBox">
+          <h3 className="subheading">MARKETING: <span className="red">10%</span></h3>
+          <h3 className="subheading">CREW: <span className="red">8%</span></h3>
+        </div>
+          <div></div>
+        </div>
       </div>
     </div>
   );
