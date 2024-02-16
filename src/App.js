5097d9e5160d26caba7c12c52a1f3949ae44ca69
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-02-16 03:38:53 +0200

    all

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
         </div>
         <button className="button">{ticker}</button>
         <div className="socialsBlock">
-          <a href="" target="_blank" rel="noopener noreferrer"><img alt="telegram" src={TG} /></a>
-          <a href="" target="_blank" rel="noopener noreferrer"><img alt="twitter" src={TW} /></a>
+          <a href="https://google.com/" target="_blank" rel="noopener noreferrer"><img alt="telegram" src={TG} /></a>
+          <a href="https://google.com/" target="_blank" rel="noopener noreferrer"><img alt="twitter" src={TW} /></a>
         </div>
       </div>
       <div className="section2">
